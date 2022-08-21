905c5f01e62e70244776e9f212f7c4b712a97bda
Razaq Mzale <rmmzale@outlook.com>
2022-08-21 20:01:25 +0300

    added intersection observer api and some keyframe animations for css

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const modelBtn = document.querySelector(".model-button");
 const bookingBtn = document.querySelector(".booking-button");
 const sections = document.querySelectorAll("section");
 const loader = document.querySelector(".loader");
+const heroContainer = document.querySelector(".hero-content-container");
+const aboutSection = document.querySelector(".about-section");
 
-console.log(sections);
 // Loading
 window.addEventListener("load", () => {
   loader.classList.add("hidden");
@@ -33,3 +34,19 @@ const smoothScrollNav = (e) => {
 navBar.addEventListener("click", smoothScrollNav);
 modelBtn.addEventListener("click", smoothScroll);
 bookingBtn.addEventListener("click", smoothScroll);
+
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.1,
+};
+
+const mainObserver = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    entry.isIntersecting
+      ? (entry.target.style.animation = "invis-to-vis 1s ease-in-out")
+      : (entry.target.style.animation = "invisible 0.5s ease-in-out");
+  });
+}, options);
+
+mainObserver.observe(heroContainer);
